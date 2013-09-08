47d30f8309e3daa096f14459c76735848fde9f52
jporwol <pako1337@gmail.com>
2013-09-08 12:47:58 +0200

    ContentElement class with Type

---
+++ ContentDomain.Test/ContentElementTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Xunit;
+using FluentAssertions;
+using Xunit.Extensions;
+
+namespace ContentDomain.Test
+{
+    public class ContentElementTest
+    {
+        [Fact]
+        public void should_be_created()
+        {
+            var contentElement = GetDefaultContentElement();
+            contentElement.Should().NotBeNull();
+        }
+
+        [Theory]
+        [InlineData(ContentType.Text)]
+        [InlineData(ContentType.List)]
+        [InlineData(ContentType.Graphic)]
+        public void should_have_type_assigned(ContentType type)
+        {
+            var contentElement = GetContentElementWithType(type);
+            contentElement.Type.Should().Be(type);
+        }
+
+        private ContentElement GetDefaultContentElement()
+        {
+            return GetContentElementWithType(ContentType.Text);
+        }
+
+        private ContentElement GetContentElementWithType(ContentType type)
+        {
+            return new ContentElement(type);
+        }
+    }
+}
---
+++ ContentDomain/ContetnElement.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContentDomain
+{
+    public class ContentElement
+    {
+        public ContentType Type { get; private set; }
+
+        public ContentElement(ContentType type)
+        {
+            Type = type;
+        }
+    }
+}
