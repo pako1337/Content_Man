97acb4e74eaafe58fd069576ced5f2ec972926b9
jporwol <pako1337@gmail.com>
2013-09-08 21:30:45 +0200

    TextContent draft implementation

---
+++ ContentDomain.Test/TextContentTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+using Xunit.Extensions;
+
+namespace ContentDomain.Test
+{
+    public class TextContentTest
+    {
+        [Fact]
+        public void should_be_created_in_draft_status()
+        {
+            var content = new TextContent();
+            content.Status.Should().Be(ContentStatus.Draft);
+        }
+
+        [Fact]
+        public void should_change_status_to_complete_on_mark_complete()
+        {
+            var content = new TextContent();
+            content.MarkComplete();
+            content.Status.Should().Be(ContentStatus.Complete);
+        }
+    }
+}
---
+++ ContentDomain/TextContent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContentDomain
+{
+    public class TextContent : IContentValue
+    {
+        public ContentStatus Status { get; private set; }
+
+        public void MarkComplete()
+        {
+            Status = ContentStatus.Complete;
+        }
+    }
+}
