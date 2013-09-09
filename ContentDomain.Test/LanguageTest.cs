230bab2f11fa5c65b043cd8c8d9882a04f7a6aab
jporwol <pako1337@gmail.com>
2013-09-09 21:25:03 +0200

    InvariantLanguage definition

---
+++ ContentDomain.Test/LanguageTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+using Xunit.Extensions;
+
+namespace ContentDomain.Test
+{
+    public class LanguageTest
+    {
+        [Fact]
+        public void should_return_the_same_object_when_getting_invariant_language()
+        {
+            var lang1 = Language.InvariantLanguage();
+            var lang2 = Language.InvariantLanguage();
+            lang1.Should().Be(lang2);
+        }
+    }
+}
