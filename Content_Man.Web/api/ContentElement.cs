9aaef9c3d0154a4b007f560487090c1828104258
jporwol <pako1337@gmail.com>
2013-09-23 20:19:40 +0200

    added ContentElement api handler

---
+++ Content_Man.Web/api/ContentElement.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Content_Man.Web.api
+{
+    public class ContentElement : Nancy.NancyModule
+    {
+        public ContentElement() : base("api/")
+        {
+        }
+    }
+}
