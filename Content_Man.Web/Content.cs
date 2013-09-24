8fd1100ce0af69097124ca5b1258166084e40687
jporwol <pako1337@gmail.com>
2013-09-24 17:58:35 +0200

    Content view web page placeholder push all changes through nancy

---
+++ Content_Man.Web/Content.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Content_Man.Web
+{
+    public class Content : Nancy.NancyModule
+    {
+        public Content() : base("/")
+        {
+            Get["Content"] = _ => View["Content"];
+        }
+    }
+}
