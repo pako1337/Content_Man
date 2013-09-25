92c09df23281b2a5137a6c201107d45dc85c4b20
jporwol <pako1337@gmail.com>
2013-09-25 18:08:15 +0200

    custom bootstraper to send javascript content

---
+++ Content_Man.Web/CustomBootstraper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy.Conventions;
+
+namespace Content_Man.Web
+{
+    public class CustomBootstraper : Nancy.DefaultNancyBootstrapper
+    {
+        protected override void ConfigureConventions(Nancy.Conventions.NancyConventions nancyConventions)
+        {
+            base.ConfigureConventions(nancyConventions);
+
+            Conventions.StaticContentsConventions.Add(
+                StaticContentConventionBuilder.AddDirectory("/Scripts"));
+        }
+    }
+}
