fae8d6edfd7c87095cab3533caedb00e027114bf
jporwol <pako1337@gmail.com>
2013-09-28 14:20:59 +0200

    return http 200 code from content element api

--- Content_Man.Web/api/ContentElement.cs
+++ Content_Man.Web/api/ContentElement.cs
@@ -2,13 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Nancy;
 
 namespace Content_Man.Web.api
 {
     public class ContentElement : Nancy.NancyModule
     {
-        public ContentElement() : base("api/")
+        public ContentElement() : base("api/ContentElement")
         {
+            Get["/"] = _ =>
+            {
+                return HttpStatusCode.OK;
+            };
         }
     }
 }
