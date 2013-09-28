1434302a5a2d8940ff1032ba5b539c0750146510
jporwol <pako1337@gmail.com>
2013-09-28 15:15:02 +0200

    api module returns sample json data

--- Content_Man.Web/api/ContentElementApiModule.cs
+++ Content_Man.Web/api/ContentElementApiModule.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using ContentDomain;
 using Nancy;
 
 namespace Content_Man.Web.api
@@ -12,7 +13,27 @@ namespace Content_Man.Web.api
         {
             Get["/"] = _ =>
             {
-                return HttpStatusCode.OK;
+                var ce = new ContentElement<TextContent>(Language.Invariant);
+                var textContent = new TextContent(Language.Invariant);
+                textContent.SetValue("text");
+                ce.SetValue(textContent);
+
+                var obj = new
+                {
+                    Id = ce.Id,
+                    DefaultLanguage = ce.DefaultLanguage,
+                    Values = ce.GetValues()
+                };
+
+                var serializer = new Nancy.Json.JavaScriptSerializer();
+                var ceString = serializer.Serialize(obj);
+                var bytes = System.Text.Encoding.UTF8.GetBytes(ceString);
+
+                return new Response()
+                    {
+                        ContentType = "application/json",
+                        Contents = s => s.Write(bytes, 0, bytes.Length)
+                    };
             };
         }
     }
