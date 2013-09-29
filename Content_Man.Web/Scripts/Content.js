4e18b5233093ab45ffec315bda4585e866b20aed
jporwol <pako1337@gmail.com>
2013-09-29 16:53:39 +0200

    move ContentEdit class outside of controller

--- Content_Man.Web/Scripts/Content.js
+++ Content_Man.Web/Scripts/Content.js
@@ -19,33 +19,33 @@ function ContentList($scope, $http) {
         .error(function (data, status, headers, config) {
             alert("Oh my! Something not nice has happened");
         });
-
-    function ContentElement(id, language, values) {
-        this.Id = id;
-        this.Values = values;
-        this.SelectedValue;
-        this.Languages = (function (values) {
-            var languages = [];
-            for (var i = 0; i < values.length; i++)
-                languages.push(values[i].Language);
-
-            return languages;
-        })(this.Values);
-
-        this.changeLanguage = function (isoCode) {
-            this.SelectedValue = findValueWithLanguage(this.Values, isoCode);
-        };
-
-        this.changeLanguage(language.IsoCode);
-
-        function findValueWithLanguage(values, isoCode) {
-            for (var i = 0; i < values.length; i++)
-                if (values[i].Language.IsoCode === isoCode)
-                    return values[i];
-        }
-    }
 }
 
 function ContentEdit($scope, $routeParams) {
     $scope.contentElement = { contentId: $routeParams.contentId, Language: 'English', Category: 'Generic', Value: "Na pokładowym chronometrze zwanej przez załogę Latającą Holerą transgalaktycznej pirackiej łajby Małpilus dochodziła godzina dwudziesta pierwsza, gdy wydarzył się ten wypadek. Sam w sobie nie miał wielkiego znaczenia - ot, drobny błąd nawigatora, cała sprawa mogłaby spokojnie obyć się bez konsekwencji. Traf chciał jednak, iż autorzy uczonych ksiąg, które miały dopiero powstać, uznali tę właśnie chwilę za przełomową w dziejach Trzeciej Ery. Nam zaś wszakże nie wypada się spierać z historią. Zamilknijmy przeto i pozwólmy jej mówić własnymi słowy... " }
+}
+
+function ContentElement(id, language, values) {
+    this.Id = id;
+    this.Values = values;
+    this.SelectedValue;
+    this.Languages = (function (values) {
+        var languages = [];
+        for (var i = 0; i < values.length; i++)
+            languages.push(values[i].Language);
+
+        return languages;
+    })(this.Values);
+
+    this.changeLanguage = function (isoCode) {
+        this.SelectedValue = findValueWithLanguage(this.Values, isoCode);
+    };
+
+    this.changeLanguage(language.IsoCode);
+
+    function findValueWithLanguage(values, isoCode) {
+        for (var i = 0; i < values.length; i++)
+            if (values[i].Language.IsoCode === isoCode)
+                return values[i];
+    }
 }
