a0381dfe76f5e93d82f0cf4e8eec280c501aeeb8
jporwol <pako1337@gmail.com>
2013-10-02 22:49:58 +0200

    get single content element api method

--- Content_Man.Web/api/ContentElementApiModule.cs
+++ Content_Man.Web/api/ContentElementApiModule.cs
@@ -45,6 +45,39 @@ namespace Content_Man.Web.api
                         Contents = s => s.Write(bytes, 0, bytes.Length)
                     };
             };
+
+            Get["/{elementId}"] = arg =>
+            {
+                var list = new List<ContentElement<TextContent>>();
+                var ce = new ContentElement<TextContent>(arg.elementId, Language.Invariant);
+                var textContent = new TextContent(Language.Invariant);
+                textContent.SetValue("text invariant");
+                ce.SetValue(textContent);
+
+                textContent = new TextContent(Language.Create("en-GB"));
+                textContent.SetValue("text in english");
+                ce.SetValue(textContent);
+
+                list.Add(ce);
+
+                var jsonModel = 
+                    new
+                    {
+                        Id = ce.Id,
+                        DefaultLanguage = ce.DefaultLanguage,
+                        Values = ce.GetValues()
+                    };
+
+                var serializer = new Nancy.Json.JavaScriptSerializer();
+                var ceString = serializer.Serialize(jsonModel);
+                var bytes = System.Text.Encoding.UTF8.GetBytes(ceString);
+
+                return new Response()
+                {
+                    ContentType = "application/json",
+                    Contents = s => s.Write(bytes, 0, bytes.Length)
+                };
+            };
         }
     }
 
