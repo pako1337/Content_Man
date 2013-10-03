58f6082c7eebffa853693f5fb4cd66a7ef0e5c79
jporwol <pako1337@gmail.com>
2013-10-03 19:42:31 +0200

    more sensible sample text

--- Content_Man.Web/api/ContentElementApiModule.cs
+++ Content_Man.Web/api/ContentElementApiModule.cs
@@ -28,11 +28,11 @@ namespace Content_Man.Web.api
 
                 ce = new ContentElement<TextContent>(1, Language.Invariant);
                 textContent = new TextContent(Language.Invariant);
-                textContent.SetValue("adsfasdfasdf asdfasdfasdf asdf asdf dasf dasfdasf");
+                textContent.SetValue("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum");
                 ce.SetValue(textContent);
 
                 textContent = new TextContent(Language.Create("en-GB"));
-                textContent.SetValue("eng dasfasdfas dfsdfas dfs dfas dfas dfas dfas");
+                textContent.SetValue("But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful");
                 ce.SetValue(textContent);
 
                 list.Add(ce);
