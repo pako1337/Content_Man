0c5a41d88a63133fb4bdcbc5813dcb59d4afdc6b
jporwol <pako1337@gmail.com>
2013-10-06 14:00:14 +0200

    added drops for easier db recreation

--- Database/CreateDatabase.sql
+++ Database/CreateDatabase.sql
@@ -1,23 +1,30 @@
-﻿CREATE TABLE "Language"
+﻿DROP TABLE IF EXISTS "TextContents";
+DROP TABLE IF EXISTS "ContentElements";
+DROP TABLE IF EXISTS "Languages";
+
+CREATE TABLE "Languages"
 (
 	"LanguageId" varchar(10) NOT NULL,
 	"Name" varchar(255) NOT NULL,
 
-	CONSTRAINT "PK_Language"
+	CONSTRAINT "PK_Languages"
 		PRIMARY KEY ("LanguageId")
 );
 
-CREATE TABLE "ContentElement"
+CREATE TABLE "ContentElements"
 (
 	"ContentElementId" serial NOT NULL,
 	"ContentType" integer NOT NULL,
 	"DefaultLanguage" varchar(10) NOT NULL,
 
-	CONSTRAINT "PK_ContentElement"
-		PRIMARY KEY ("ContentElementId")
+	CONSTRAINT "PK_ContentElements"
+		PRIMARY KEY ("ContentElementId"),
+	CONSTRAINT "FK_ContentElements_Languages"
+		FOREIGN KEY ("DefaultLanguage")
+		REFERENCES "Languages"("LanguageId")
 );
 
-CREATE TABLE "TextContent"
+CREATE TABLE "TextContents"
 (
 	"ContentValueId" serial NOT NULL,
 	"ContentElementId" integer NOT NULL,
@@ -25,9 +32,12 @@ CREATE TABLE "TextContent"
 	"Language" varchar(10) NOT NULL,
 	"Value" text,
 
-	CONSTRAINT "PK_TextContent"
+	CONSTRAINT "PK_TextContents"
 		PRIMARY KEY ("ContentValueId"),
-	CONSTRAINT "FK_TextContent_ContentElement"
+	CONSTRAINT "FK_TextContents_ContentElements"
 		FOREIGN KEY ("ContentElementId")
-		REFERENCES "ContentElement"("ContentElementId")
+		REFERENCES "ContentElements"("ContentElementId"),
+	CONSTRAINT "FK_TextContents_Languages"
+		FOREIGN KEY ("Language")
+		REFERENCES "Languages"("LanguageId")
 );
