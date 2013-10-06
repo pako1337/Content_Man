2ce168fab5e96f48a31322d9d2c2e8d0f4de5173
jporwol <pako1337@gmail.com>
2013-10-06 19:06:18 +0200

    removed duplication in code

--- ContentDomain.Test/ContentElementFactoryTest.cs
+++ ContentDomain.Test/ContentElementFactoryTest.cs
@@ -3,39 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Xunit;
-using FluentAssertions;
 using ContentDomain.Factories;
+using FluentAssertions;
+using Xunit;
 
 namespace ContentDomain.Test
 {
     public class ContentElementFactoryTest
     {
+        private ContentElementFactory factory = new ContentElementFactory();
+
         [Fact]
         public void should_create_new_ContentElement()
         {
-            var factory = new ContentElementFactory();
             factory.Create(Language.InvariantCode, ContentType.Text).Should().NotBeNull();
         }
 
         [Fact]
         public void should_have_id_set_to_minus_1()
         {
-            var factory = new ContentElementFactory();
             factory.Create(Language.InvariantCode, ContentType.Text).ContentElementId.Should().Be(-1);
         }
 
         [Fact]
         public void should_have_type_set_correctly()
         {
-            var factory = new ContentElementFactory();
             factory.Create(Language.InvariantCode, ContentType.List).ContentType.Should().Be(ContentType.List);
         }
 
         [Fact]
         public void should_have_language_set_correctly()
         {
-            var factory = new ContentElementFactory();
             factory.Create("en", ContentType.List).DefaultLanguage.Should().Be(Language.Create("en"));
         }
     }
