6fe2343677014e396c24360ac050785796ebd75d
jporwol <pako1337@gmail.com>
2013-10-10 22:34:21 +0200

    insert logic for ContentElement

--- ContentDomain/Repositories/ContentElementRepository.cs
+++ ContentDomain/Repositories/ContentElementRepository.cs
@@ -32,6 +32,23 @@ namespace ContentDomain.Repositories
                 .WithTextContents();
             return factory.Create(elements.FirstOrDefault());
         }
+
+        public void Insert(ContentElement contentElement)
+        {
+            var contentElementDb = new ContentElementDb(contentElement);
+            var db = Database.Open();
+            using (var tx = db.BeginTransaction())
+            {
+                var ce = tx.ContentElements.Insert(contentElementDb);
+                foreach (var textContent in contentElementDb.TextContents)
+                {
+                    textContent.ContentElementId = ce.ContentElementId;
+                    tx.TextContents.Insert(textContent);
+                }
+
+                tx.Commit();
+            }
+        }
     }
 
     internal class ContentElementDb
@@ -40,13 +57,41 @@ namespace ContentDomain.Repositories
         public ContentType ContentType { get; set; }
         public string DefaultLanguage { get; set; }
         public List<TextContentDb> TextContents { get; set; }
+
+        public ContentElementDb()
+        { }
+
+        public ContentElementDb(ContentElement ce)
+        {
+            ContentElementId = ce.ContentElementId;
+            ContentType = ce.ContentType;
+            DefaultLanguage = ce.DefaultLanguage.LanguageId;
+            TextContents = ce
+                .GetValues()
+                .OfType<TextContent>()
+                .Select(v => new TextContentDb(v, this))
+                .ToList();
+        }
     }
 
     internal class TextContentDb
     {
         public int TextContentId { get; set; }
-        public int ContentStatus { get; set; }
+        public int ContentElementId { get; set; }
+        public ContentStatus ContentStatus { get; set; }
         public string Value { get; set; }
         public string Language { get; set; }
+
+        public TextContentDb()
+        { }
+
+        public TextContentDb(TextContent tc, ContentElementDb ce)
+        {
+            TextContentId = tc.ContentValueId;
+            ContentElementId = ce.ContentElementId;
+            ContentStatus = tc.Status;
+            Value = tc.Value;
+            Language = tc.Language.LanguageId;
+        }
     }
 }
