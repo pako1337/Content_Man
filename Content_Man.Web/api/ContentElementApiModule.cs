d99a67a90296887d04aa1e92ccdb50eaaa076e31
jporwol <pako1337@gmail.com>
2013-10-16 20:48:17 +0200

    using Nancy built in methods to return json

--- Content_Man.Web/api/ContentElementApiModule.cs
+++ Content_Man.Web/api/ContentElementApiModule.cs
@@ -18,31 +18,13 @@ namespace Content_Man.Web.api
             Get["/"] = _ =>
             {
                 var list = new ContentElementRepository().All().AsDto();
-
-                var serializer = new Nancy.Json.JavaScriptSerializer();
-                var ceString = serializer.Serialize(list);
-                var bytes = System.Text.Encoding.UTF8.GetBytes(ceString);
-
-                return new Response()
-                    {
-                        ContentType = "application/json",
-                        Contents = s => s.Write(bytes, 0, bytes.Length)
-                    };
+                return Response.AsJson<IEnumerable<ContentElementDto>>(list, HttpStatusCode.OK);
             };
 
             Get["/{elementId}"] = arg =>
             {
                 var jsonModel = new ContentElementRepository().Get((int)arg.elementId).AsDto();
-
-                var serializer = new Nancy.Json.JavaScriptSerializer();
-                var ceString = serializer.Serialize(jsonModel);
-                var bytes = System.Text.Encoding.UTF8.GetBytes(ceString);
-
-                return new Response()
-                {
-                    ContentType = "application/json",
-                    Contents = s => s.Write(bytes, 0, bytes.Length)
-                };
+                return Response.AsJson<ContentElementDto>(jsonModel, HttpStatusCode.OK);
             };
 
             Post["/"] = _ =>
