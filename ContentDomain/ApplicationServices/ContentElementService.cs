7db60bf33eab24540a5ac19bee323cd5162a969b
jporwol <pako1337@gmail.com>
2013-10-16 21:13:37 +0200

    ContentElement business service

---
+++ ContentDomain/ApplicationServices/ContentElementService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ContentDomain.Dto;
+using ContentDomain.Repositories;
+
+namespace ContentDomain.ApplicationServices
+{
+    public class ContentElementService
+    {
+        public void InsertNewContentElement(ContentElementDto contentElement)
+        {
+            var repo = new ContentElementRepository();
+            try
+            {
+                var ce = new ContentDomain.Factories.ContentElementFactory().Create(contentElement);
+                repo.Insert(ce);
+            }
+            catch (ArgumentException ae)
+            {
+            }
+        }
+    }
+}
