b15e3105f8f09e2e72b19d48283c9949a16ff29d
jporwol <pako1337@gmail.com>
2013-10-17 21:54:52 +0200

    created DomainException

---
+++ ContentDomain/DomainException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ContentDomain
+{
+    public class DomainException : Exception
+    {
+        public DomainException(string message)
+            : base(message)
+        {
+        }
+    }
+}
