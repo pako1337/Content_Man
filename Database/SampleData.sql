d6c04fb567afcc9a8fe8c704a8035224b67f3e2f
jporwol <pako1337@gmail.com>
2013-10-26 17:23:59 +0200

    removed empty content elements from sample data

--- Database/SampleData.sql
+++ Database/SampleData.sql
@@ -6,13 +6,11 @@
 
 INSERT INTO "ContentElements" ("ContentType", "DefaultLanguage") VALUES
 	(1, 'en'),
-	(1, 'en'),
-	(1, 'pl'),
-	(1, 'Invariant');
+	(1, 'pl');
 
 INSERT INTO "TextContents" ("ContentElementId", "ContentStatus", "Language", "Value") VALUES
 	(1, 1, 'en', 'english sample'),
 	(1, 1, 'pl', 'polski przykład'),
 	(1, 1, 'Invariant', 'Invariant sample'),
-	(3, 1, 'pl', 'Litwo, ojczyzno moja'),
-	(3, 1, 'en', 'O Lithuania, my country');
+	(2, 1, 'pl', 'Litwo, ojczyzno moja'),
+	(2, 1, 'en', 'O Lithuania, my country');
