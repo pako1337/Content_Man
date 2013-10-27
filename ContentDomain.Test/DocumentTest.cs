7bbdbd6275aae713a0d7203d9ca3880db0b9c86a
jporwol <pako1337@gmail.com>
2013-10-27 13:37:03 +0100

    created document class

---
+++ ContentDomain.Test/DocumentTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Extensions;
+using FluentAssertions;
+
+namespace ContentDomain.Test
+{
+    public class DocumentTest
+    {
+        [Fact]
+        public void should_have_open_status_by_default()
+        {
+            var doc = new Document();
+            doc.Status.Should().Be(DocumentStatus.Open);
+        }
+    }
+}
---
+++ ContentDomain/Document.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContentDomain
+{
+    public class Document
+    {
+        public int DocumentId { get; set; }
+        public DocumentStatus Status { get; set; }
+
+        public Document()
+        {
+            Status = DocumentStatus.Open;
+        }
+    }
+}
