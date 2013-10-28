0c2fcdc9a4298963c2357dd94e766faffd142672
jporwol <pako1337@gmail.com>
2013-10-28 20:27:06 +0100

    free section draft

---
+++ ContentDomain.Test/FreeSectionTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Extensions;
+using FluentAssertions;
+using ContentDomain.ContentContext;
+using ContentDomain.Shared;
+using ContentDomain.DocumentContext;
+
+namespace ContentDomain.Test
+{
+    public class FreeSectionTest
+    {
+        [Fact]
+        public void should_accept_any_content()
+        {
+            var section = new FreeSection();
+            section.AddContent(new ContentElement(-1, Language.Invariant, ContentType.Text));
+            section.GetContent().Count().Should().Be(1);
+        }
+    }
+}
---
+++ ContentDomain/DocumentContext/FreeSection.cs
@@ -0,0 +1,28 @@
+﻿using ContentDomain.ContentContext;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContentDomain.DocumentContext
+{
+    public class FreeSection : IDocumentSection
+    {
+        public List<ContentElement> _content = new List<ContentElement>();
+
+        public int SectionId { get { return 1; } }
+        public string Name { get { return ""; } }
+
+        public void AddContent(ContentContext.ContentElement contentElement)
+        {
+            _content.Add(contentElement);
+        }
+
+        public IImmutableList<ContentContext.ContentElement> GetContent()
+        {
+            return _content.ToImmutableList();
+        }
+    }
+}
