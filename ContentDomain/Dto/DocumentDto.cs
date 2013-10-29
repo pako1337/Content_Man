e7577bdfe6ce13efc94605a1925546eb9e715ee2
jporwol <pako1337@gmail.com>
2013-10-29 18:21:26 +0100

    draft version of document dto classes

---
+++ ContentDomain/Dto/DocumentDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContentDomain.Dto
+{
+    public class DocumentDto
+    {
+        public int DocumentId { get; set; }
+        public string Name { get; set; }
+        public int Status { get; set; }
+        public List<SectionDto> Sections { get; set; }
+    }
+}
---
+++ ContentDomain/Dto/SectionDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContentDomain.Dto
+{
+    public class SectionDto
+    {
+        public int SectionId { get; set; }
+        public int Type { get; set; }
+        public string Name { get; set; }
+        public List<ContentElementDto> ContentElements { get; set; }
+    }
+}
